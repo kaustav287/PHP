d27d3a927ca48ff72b524f9d6311f0827340e462
kaustav287 <64247065+kaustav287@users.noreply.github.com>
2020-07-16 20:57:27 +0530

    updated classes.php

--- cms_proj/classes.php
+++ cms_proj/classes.php
@@ -30,7 +30,6 @@ public function storeFormValues ( $params ) {
     
     $this->__construct( $params );
 
-    // Parse and store the publication date
     if ( isset($params['publicationDate']) ) {
       $publicationDate = explode ( '-', $params['publicationDate'] );
 
@@ -52,4 +51,93 @@ public function storeFormValues ( $params ) {
     $pdo = null;
 
     return new Article($row);
-  }}
+  }
+
+
+public static function getList( $numRows=1000000 ) {
+    $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
+    $sql = "SELECT SQL_CALC_FOUND_ROWS *, UNIX_TIMESTAMP(publicationDate) AS publicationDate FROM articles
+            ORDER BY publicationDate DESC LIMIT :numRows";
+
+    $st = $conn->prepare( $sql );
+    $st->bindValue( ":numRows", $numRows, PDO::PARAM_INT );
+    $st->execute();
+    $list = array();
+
+    while ( $row = $st->fetch() ) {
+      $article = new Article( $row );
+      $list[] = $article;
+    }
+
+    // Now get the total number of articles that matched the criteria
+    $sql = "SELECT FOUND_ROWS() AS totalRows";
+    $totalRows = $conn->query( $sql )->fetch();
+    $conn = null;
+    return ( array ( "results" => $list, "totalRows" => $totalRows[0] ) );
+  }
+
+
+  /**
+  * Inserts the current Article object into the database, and sets its ID property.
+  */
+
+  public function insert() {
+
+    // Does the Article object already have an ID?
+    if ( !is_null( $this->id ) ) trigger_error ( "Article::insert(): Attempt to insert an Article object that already has its ID property set (to $this->id).", E_USER_ERROR );
+
+    // Insert the Article
+    $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
+    $sql = "INSERT INTO articles ( publicationDate, title, summary, content ) VALUES ( FROM_UNIXTIME(:publicationDate), :title, :summary, :content )";
+    $st = $conn->prepare ( $sql );
+    $st->bindValue( ":publicationDate", $this->publicationDate, PDO::PARAM_INT );
+    $st->bindValue( ":title", $this->title, PDO::PARAM_STR );
+    $st->bindValue( ":summary", $this->summary, PDO::PARAM_STR );
+    $st->bindValue( ":content", $this->content, PDO::PARAM_STR );
+    $st->execute();
+    $this->id = $conn->lastInsertId();
+    $conn = null;
+  }
+
+
+  /**
+  * Updates the current Article object in the database.
+  */
+
+  public function update() {
+
+    // Does the Article object have an ID?
+    if ( is_null( $this->id ) ) trigger_error ( "Article::update(): Attempt to update an Article object that does not have its ID property set.", E_USER_ERROR );
+   
+    // Update the Article
+    $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
+    $sql = "UPDATE articles SET publicationDate=FROM_UNIXTIME(:publicationDate), title=:title, summary=:summary, content=:content WHERE id = :id";
+    $st = $conn->prepare ( $sql );
+    $st->bindValue( ":publicationDate", $this->publicationDate, PDO::PARAM_INT );
+    $st->bindValue( ":title", $this->title, PDO::PARAM_STR );
+    $st->bindValue( ":summary", $this->summary, PDO::PARAM_STR );
+    $st->bindValue( ":content", $this->content, PDO::PARAM_STR );
+    $st->bindValue( ":id", $this->id, PDO::PARAM_INT );
+    $st->execute();
+    $conn = null;
+  }
+
+
+  /**
+  * Deletes the current Article object from the database.
+  */
+
+  public function delete() {
+
+    // Does the Article object have an ID?
+    if ( is_null( $this->id ) ) trigger_error ( "Article::delete(): Attempt to delete an Article object that does not have its ID property set.", E_USER_ERROR );
+
+    // Delete the Article
+    $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
+    $st = $conn->prepare ( "DELETE FROM articles WHERE id = :id LIMIT 1" );
+    $st->bindValue( ":id", $this->id, PDO::PARAM_INT );
+    $st->execute();
+    $conn = null;
+  }
+
+}
